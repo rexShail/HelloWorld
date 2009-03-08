eeb0b637189531dcc2a2529b87d110d8397fc7e7
rexShailJs <rexshailjs@gmail.com>
2009-03-08 12:35:05 -0400

    rexshail codeigniter

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "Hello, World!";
-?>
+require_once __DIR__ . '/application/core/Loader.php';
+require_once __DIR__ . '/application/core/CI_Controller.php';
+require_once __DIR__ . '/application/controllers/Welcome.php';
+
+$controller = new Welcome();
+$controller->index();
